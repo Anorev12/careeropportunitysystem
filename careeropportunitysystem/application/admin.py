02480d08944d50dbdf142396620d23100ce75430
Kurt <167537767+Icession@users.noreply.github.com>
2026-04-09 22:49:11 +0800

    Fixes some minor problems

--- careeropportunitysystem/application/admin.py
+++ careeropportunitysystem/application/admin.py
@@ -4,7 +4,7 @@ from .models import Application
 
 @admin.register(Application)
 class ApplicationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'applicant', 'job_posting', 'application_date', 'status')
+    list_display = ('id', 'application_date', 'status')
     list_filter = ('status', 'application_date')
     search_fields = ('applicant__address', 'remarks')
     ordering = ('-application_date',)
