bdaa29c2a93fb9cb5de7be3f3fc1f9ad14b0b2ec
AndrieFranza <afranza2004@gmail.com>
2026-04-09 23:53:32 +0800

    adding something

--- careeropportunitysystem/application/urls.py
+++ careeropportunitysystem/application/urls.py
@@ -1,10 +1,14 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from django.contrib.auth import views as auth_views
+from accounts.views import index_view  # Make sure this import matches your app name
 
 urlpatterns = [
-    path('', views.application_list, name='application_list'),
-    path('<int:pk>/', views.application_detail, name='application_detail'),
-    path('create/', views.application_create, name='application_create'),
-    path('<int:pk>/update/', views.application_update, name='application_update'),
-    path('<int:pk>/delete/', views.application_delete, name='application_delete'),
+    path('admin/', admin.site.urls),
+
+    # This fixes the Index page (127.0.0.1:8080/)
+    path('', index_view, name='index'),
+
+    # This fixes the Login page (127.0.0.1:8080/login/)
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
--- careeropportunitysystem/careeropportunitysystem/settings.py
+++ careeropportunitysystem/careeropportunitysystem/settings.py
@@ -79,10 +79,10 @@ WSGI_APPLICATION = 'careeropportunitysystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbcareeropportunitysystem',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'Abebea2004',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
