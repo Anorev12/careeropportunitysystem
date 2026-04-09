1f13635c5165042ac1b6cefcdeb7b145949b9bae
AndrieFranza <afranza2004@gmail.com>
2026-04-09 22:29:37 +0800

    first commit by andrie franza

--- careeropportunitysystem/application/admin.py
+++ careeropportunitysystem/application/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Application
 
-# Register your models here.
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'applicant', 'job_posting', 'application_date', 'status')
+    list_filter = ('status', 'application_date')
+    search_fields = ('applicant__address', 'remarks')
+    ordering = ('-application_date',)
