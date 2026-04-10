ef628ce67711089593f0def0a5276a2634b7f6c6
davidgabutan <davidgabutan225@gmail.com>
2026-04-10 21:44:53 +0800

    changes a lot

--- careeropportunitysystem/urls.py
+++ careeropportunitysystem/urls.py
@@ -2,19 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.shortcuts import render
+from django.shortcuts import redirect
 
-def home(request):
-    return render(request, 'home.html')
-
+# Root URL (127.0.0.1:8080/) redirects to the accounts index page
 urlpatterns = [
     path('admin/',         admin.site.urls),
-    path('',               home,                        name='home'),
-    path('accounts/',      include('accounts.urls')),
+    path('',               lambda req: redirect('accounts:index'), name='home'),
+    path('accounts/',      include('accounts.urls',      namespace='accounts')),
     path('employer/',      include('employer.urls')),
-    path('application/',   include('application.urls')),
     path('bookmarks/',     include('bookmarks.urls')),
     path('notifications/', include('notifications.urls')),
+    path('application/',   include('application.urls')),
 ]
 
 if settings.DEBUG:
