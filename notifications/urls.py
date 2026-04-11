de519001f429863bc748f458683069294c9c17a1
Kurt <167537767+Icession@users.noreply.github.com>
2026-04-11 10:11:34 +0800

    Changed notifiaction and added login

--- notifications/urls.py
+++ notifications/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.notifications, name='notifications'),
+    path('login/', views.notifications_login, name='notifications_login'),
 ]
--- notifications/views.py
+++ notifications/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+def notifications_login(request):
+    if request.method == 'POST':
+        return redirect('/notifications/')
+    return render(request, 'careeropportunitysystem/login.html')
 
 def notifications(request):
     return render(request, 'careeropportunitysystem/notifications.html')
